28f726bbe65f5172a91e771021fae990a2cff02d
happyVee <30397174+happyVee@users.noreply.github.com>
2017-07-27 21:51:46 +0800

    sort url
    only four params are need to query info

--- marriott.py
+++ marriott.py
@@ -20,15 +20,15 @@ _headers = {
 resp = requests.get(_url, headers= _headers)
 cookies = resp.cookies    
 
-url ="http://www.marriott.com.cn/search/submitSearch.mi?searchType=InCity&groupCode=&searchRadius=&poiName=&poiCity=%E5%8C%97%E4%BA%AC&recordsPerPage=20&for-hotels-nearme=%E9%9D%A0%E8%BF%91&destinationAddress.destination=%E5%8C%97%E4%BA%AC%2C+China&singleSearch=true&singleSearchAutoSuggest=false&autoSuggestItemType=Unmatched&clickToSearch=false&destinationAddress.latitude=39.906011&destinationAddress.longitude=116.387911&destinationAddress.cityPopulation=0.0&destinationAddress.cityPopulationDensity=0.0&destinationAddress.city=%E5%8C%97%E4%BA%AC&destinationAddress.stateProvince=&destinationAddress.country=CN&airportCode=&fromToDate=7+%E6%9C%88+28%E6%97%A5%2C+%E5%91%A8%E5%9B%9B&fromToDate_submit=2017-07-24&fromDate=2017-07-29&toDate=2017-07-30&lengthOfStay=1&roomCountBox=1&roomCount=1&guestCountBox=1&guestCount=1&clusterCode=none&corporateCode="
+url = "http://www.marriott.com.cn/search/submitSearch.mi?destinationAddress.destination=上海&fromDate=2017-07-29&toDate=2017-07-30&lengthOfStay=1&recordsPerPage=40"
 basic_url = "http://www.marriott.com.cn/search/submitSearch.mi"
 r = requests.get (url, headers = _headers,  cookies = cookies)
 #print(r.text)
-#body = html.fromstring(r.text)
+body = html.fromstring(r.text)
 
 #sub = body.xpath('//span[@class="t-price"]')
 #print(sub[0].text)
-'''
+
 soup = BeautifulSoup(r.text, 'lxml')
 
 #print(soup.prettify())
@@ -46,8 +46,8 @@ for item in items:
 	#hotel['address'] = item.find(class_= "m-hotel-address").text.strip().replace('\t','').replace('\n','')
 	hotel['price'] = int(item.find(class_ = "t-price").text.strip().replace(',',''))
 	hotels.append(hotel)
-
-#print(hotels)
+print('一共有:'+str(len(hotels))+'家')
+print(hotels)
 '''
 parsed_url = urllib.parse.urlparse(url)
 params = urllib.parse.parse_qs(parsed_url.query,True)
@@ -70,7 +70,7 @@ r_sh = requests.get (basic_url, data = params,  headers = _headers,  cookies = c
 soup = BeautifulSoup(r_sh.text, 'lxml')
 
 #print(soup.prettify())
-'''
+
 table = soup.find(id = 'comparison-form')
 
 items = table.find_all(_class = "merch-property-records")
@@ -84,8 +84,15 @@ for item in items:
 	#hotel['address'] = item.find(class_= "m-hotel-address").text.strip().replace('\t','').replace('\n','')
 	hotel['price'] = int(item.find(class_ = "t-price").text.strip().replace(',',''))
 	hotels.append(hotel)
-'''
 url_params = urllib.parse.urlencode(params)
 new_url = basic_url + "?" + url_params
 print(new_url)
 print(hotels)
+
+lengthOfStay=1
+#url ="http://www.marriott.com.cn/search/submitSearch.mi?searchType=InCity&groupCode=&searchRadius=&poiName=&poiCity=%E5%8C%97%E4%BA%AC&recordsPerPage=20&for-hotels-nearme=%E9%9D%A0%E8%BF%91&destinationAddress.destination=%E5%8C%97%E4%BA%AC%2C+China&singleSearch=true&singleSearchAutoSuggest=false&autoSuggestItemType=Unmatched&clickToSearch=false&destinationAddress.latitude=39.906011&destinationAddress.longitude=116.387911&destinationAddress.cityPopulation=0.0&destinationAddress.cityPopulationDensity=0.0&destinationAddress.city=%E5%8C%97%E4%BA%AC&destinationAddress.stateProvince=&destinationAddress.country=CN&airportCode=&fromToDate=7+%E6%9C%88+28%E6%97%A5%2C+%E5%91%A8%E5%9B%9B&fromToDate_submit=2017-07-24&fromDate=2017-07-29&toDate=2017-07-30&lengthOfStay=1&roomCountBox=1&roomCount=1&guestCountBox=1&guestCount=1&clusterCode=none&corporateCode="
+url = "http://www.marriott.com.cn/search/submitSearch.mi?destinationAddress.destination=%E5%8C%97%E4%BA%AC%2C+China&fromDate=2017-07-29&toDate=2017-07-30&lengthOfStay=1&recordsPerPage=40"
+
+#&singleSearch=true&singleSearchAutoSuggest=false&autoSuggestItemType=Unmatched&clickToSearch=false
+#&destinationAddress.latitude=39.906011&destinationAddress.longitude=116.387911&destinationAddress.cityPopulation=0.0&destinationAddress.cityPopulationDensity=0.0&destinationAddress.city=北京&destinationAddress.country=CN
+'''
