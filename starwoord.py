ec47ee9ede17504c5d7e63a891aa43028c265e91
Eric Xiao <eric.xiao01@sap.com>
2017-08-04 17:31:05 +0800

    change starwood query

--- starwoord.py
+++ starwoord.py
@@ -4,15 +4,15 @@ from urllib import parse as urlparse
 from lxml import html
 from bs4 import BeautifulSoup
 
-_url = 'http://www.marriott.com.cn/search/findHotels.mi'
+_url = 'http://www.starwoodhotels.com/preferredguest/search/index.html'
 
 _headers = {
 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 'Accept-Encoding':'gzip, deflate, sdch',
 'Accept-Language':'zh-CN,zh;q=0.8',
 'Connection':'keep-alive',
-'Host':'www.marriott.com.cn',
-'Referer':'http://www.marriott.com.cn/default.mi',
+'Host':'www.starwoodhotels.com',
+'Referer':'http://www.starwoodhotels.com',
 'Upgrade-Insecure-Requests':'1',
 'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 }
@@ -20,15 +20,17 @@ _headers = {
 resp = requests.get(_url, headers= _headers)
 cookies = resp.cookies    
 
-url = "http://www.marriott.com.cn/search/submitSearch.mi?destinationAddress.destination=China&fromDate=2017-07-29&toDate=2017-07-30&lengthOfStay=1&recordsPerPage=40&clusterCode=oxd"
-basic_url = "http://www.marriott.com.cn/search/submitSearch.mi"
+
+url = 'http://www.starwoodhotels.com/preferredguest/search/results/detail.html?country=CN&stateProvince=SHA&city=Shanghai&arrivalDate=17%E5%B9%B408%E6%9C%8823%E6%97%A5&departureDate=17%E5%B9%B408%E6%9C%8824%E6%97%A5'
+#url = "http://www.marriott.com.cn/search/submitSearch.mi?destinationAddress.destination=China&fromDate=2017-07-29&toDate=2017-07-30&lengthOfStay=1&recordsPerPage=40&clusterCode=oxd"
+#basic_url = "http://www.marriott.com.cn/search/submitSearch.mi"
 r = requests.get (url, headers = _headers,  cookies = cookies)
 
 body = html.fromstring(r.text)
 soup = BeautifulSoup(r.text, 'lxml')
-
+print(soup)
 #print(soup.prettify())
-
+'''
 table = soup.find(id = 'comparison-form')
 items = table.find_all(_class = "merch-property-records")
 items = table.select(".merch-property-records")
@@ -46,3 +48,5 @@ print(hotels)
 
 
 url = 'http://www.starwoodhotels.com/preferredguest/search/results/detail.html?country=CN&stateProvince=SHA&city=Shanghai&arrivalDate=17%E5%B9%B408%E6%9C%8823%E6%97%A5&departureDate=17%E5%B9%B408%E6%9C%8824%E6%97%A5'
+
+'''
